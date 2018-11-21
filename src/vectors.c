a6b4e7f102e1411a7b06452a3871c0973601e4ec
alex <aleksey.maksymyshyn@gmail.com>
2018-11-21 21:29:43 +0200

    Add vectors

--- src/vectors.c
+++ src/vectors.c
@@ -1,12 +1,44 @@
 #include <stdint.h>
 
-void Reset_IRQHandler(void) __attribute__(weak);
-void NMI_IRQHandler(void);
-void HardFault_IRQHandler(void);
-void MemManage_IRQHandler(void);
-void BusFault_IRQHandler(void);
-void UsageFault_IRQHandler(void);
-void SVCall_IRQHandler(void);
-void DebugMonitor_IRQHandler(void);
-void PendSV_IRQHandler(void);
-void SysTick_IRQHandler(void);
+extern uint32_t stack_pointer; 
+
+void DefaultHandler(void) __attribute__((weak));
+void DefaultHandler(void)
+{
+    while (1)
+    {
+        //simple hook
+    }
+}
+
+void Reset_IRQHandler(void)        __attribute__((weak, alias("DefaultHandler")));
+void NMI_IRQHandler(void)          __attribute__((weak, alias("DefaultHandler")));
+void HardFault_IRQHandler(void)    __attribute__((weak, alias("DefaultHandler")));
+void MemManage_IRQHandler(void)    __attribute__((weak, alias("DefaultHandler")));
+void BusFault_IRQHandler(void)     __attribute__((weak, alias("DefaultHandler")));
+void UsageFault_IRQHandler(void)   __attribute__((weak, alias("DefaultHandler")));
+void SVCall_IRQHandler(void)       __attribute__((weak, alias("DefaultHandler")));
+void DebugMonitor_IRQHandler(void) __attribute__((weak, alias("DefaultHandler")));
+void PendSV_IRQHandler(void)       __attribute__((weak, alias("DefaultHandler")));
+void SysTick_IRQHandler(void)      __attribute__((weak, alias("DefaultHandler")));
+
+ __attribute__ ((section(".vectors"))) uint32_t  vectors[] =
+{
+    0, //FIX stack pointer
+    &(Reset_IRQHandler),
+    &(NMI_IRQHandler),
+    &(HardFault_IRQHandler),
+    &(MemManage_IRQHandler),
+    &(BusFault_IRQHandler),
+    &(UsageFault_IRQHandler),
+    0,
+    0,
+    0,
+    0,
+    0,
+    &(SVCall_IRQHandler),
+    &(DebugMonitor_IRQHandler),
+    &(PendSV_IRQHandler),
+    &(SysTick_IRQHandler)
+};
+
